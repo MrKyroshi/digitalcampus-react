a1efb4cfaa4aa58d148667dc2da95f0085bcb1fc
MrKyroshi <mrkyroshi@gmail.com>
2023-02-09 14:39:55 +0100

    Final exos

--- src/Composants/Home.jsx
+++ src/Composants/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import logo from '../Composants/Asset/IMG/logo192.png'
 
 export default function Home() {
   return (
     <div className='exoCss1'>
-        <h1 style={{backgroundColor: 'aqua'}}  className='exoCss'>HOME - ExoCss</h1>
-        <h1 style={{backgroundColor: 'pink'}}  className='exoCss'>HOME - ExoCss</h1>
-        <h1 style={{backgroundColor: 'yellow'}} className='exoCss'>HOME - ExoCss</h1>
+       <h1 style={{backgroundColor: 'pink' ,backgroundImage: `url(${logo})` ,backgroundSize: 'cover', 
+                backgroundPosition: 'center center',
+                backgroundRepeat: 'no-repeat', }}  className='exoCss'>HOME - ExoCss</h1>
+        <h1 style={{backgroundColor: 'red',backgroundImage: `url(${logo})` ,backgroundSize: 'cover', 
+                backgroundPosition: 'center center',
+                backgroundRepeat: 'no-repeat',}}  className='exoCss'>HOME - ExoCss</h1>
+        <h1 style={{backgroundColor: 'be182c',backgroundImage: `url(${logo})` ,backgroundSize: 'cover', 
+                backgroundPosition: 'center center',
+                backgroundRepeat: 'no-repeat',}}  className='exoCss'>HOME - ExoCss</h1>
         <h1 style={{backgroundColor: 'f24e1e'}} className='exoCss'>HOME - ExoCss</h1>
         <h1 style={{backgroundColor: 'pink'}} className='exoCss'>HOME - ExoCss</h1>
         <h1 style={{backgroundColor: 'pink'}} className='exoCss'>HOME - ExoCss</h1>
--- src/Composants/NavBar.jsx
+++ src/Composants/NavBar.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../Composants/Asset/IMG/logo192.png'
 
 
 function NavBar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">DC Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img src={logo} width='50em' alt="logo" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
